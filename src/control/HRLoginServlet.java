2a75a88ecbfa1db5673d000b814634e92c4bd727
菖蒲元子 <sxg514@163.com>
2017-06-21 16:13:27 +0800

    initial

---
+++ src/control/HRLoginServlet.java
@@ -0,0 +1,17 @@
+package control;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Charlotte514 on 2017/6/21.
+ */
+public class HRLoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
