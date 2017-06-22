6f527a735dc01fa8eb2e67eae956a5fed88c5514
菖蒲元子 <sxg514@163.com>
2017-06-22 09:23:53 +0800

    完成HR注册登录界面的JS

--- web/js/HRLoginJS.js
+++ web/js/HRLoginJS.js
@@ -2,5 +2,11 @@
  * Created by Charlotte514 on 2017/6/21.
  */
 function HRLogin(type){
-    var formSignIn=document.getElementById("")
+    var formSignIn=document.getElementById("HR_SIGN_IN");
+    var formSignUp=document.getElementById("HR_SIGN_UP");
+    if(type==1){
+        formSignIn.submit();
+    }else if(type==2){
+        formSignUp.submit();
+    }
 }
